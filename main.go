0245b654fe46cf91219dfc5c928ffb49242894f0
Q <matthew@Qs-MacBook-Pro-Personal.local>
2024-05-17 16:32:54 +0900

    add file upload handler

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/QiuMatthew/website-backend/utils/example"
+	"github.com/QiuMatthew/website-backend/utils/fileupload"
 )
 
 func main() {
 	fmt.Println(example.Hello())
+
+	http.HandleFunc("/upload", fileupload.UploadHandler)
+	http.ListenAndServe(":8080", nil)
 }
--- utils/fileupload/fileupload.go
+++ utils/fileupload/fileupload.go
@@ -1,1 +1,30 @@
 package fileupload
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(10 << 20) // up to 10 MegaBytes
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	savePath := "./uploads"
+	err = os.MkdirAll(savePath, os.ModePerm)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	fmt.Fprintf(w, "file uploaded successfully: %s\n", handler.Filename)
+}
