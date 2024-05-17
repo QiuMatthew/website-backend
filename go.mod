eaadef97888be471d0ec0471e25ec1ea8dd7374a
Q <matthew@Qs-MacBook-Pro-Personal.local>
2024-05-17 15:01:45 +0900

    test package import

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/QiuMatthew/website-backend
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/QiuMatthew/website-backend/utils/example"
+)
+
+func main() {
+	fmt.Println(example.Hello())
+}
